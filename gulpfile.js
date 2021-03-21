9bd9e4a05d41de62d71a06e10fcf02e407049cf6
ves-sev <newsecretword@yandex.ru>
2021-03-21 21:59:26 +0300

    html and styles added

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function images(){
 
 function scripts(){
   return src([
-    'node_modules/jquery/dist/jquery.js',
+    // 'node_modules/jquery/dist/jquery.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
